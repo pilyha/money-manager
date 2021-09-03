d8b9d0bf2749a548cf50e87b5bea5cc1f5f5faf5
pilyha <illia.popov57@gmail.com>
2021-09-04 01:24:49 +0300

    add maven-wrapper.properties

---
+++ src/main/java/com/project/moneymanager/controllers/NoteController.java
@@ -0,0 +1,42 @@
+package com.project.moneymanager.controllers;
+
+import com.project.moneymanager.models.Note;
+import com.project.moneymanager.models.User;
+import com.project.moneymanager.services.NoteService;
+import com.project.moneymanager.services.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.security.Principal;
+
+@Controller
+@RequestMapping("/notes")
+public class NoteController {
+
+    private final NoteService noteService;
+    private final UserService userService;
+
+    public NoteController(NoteService noteService, UserService userService) {
+        this.noteService = noteService;
+        this.userService = userService;
+    }
+
+    @PostMapping(value = "/new")
+    public String newNote(Principal principal, @Valid @ModelAttribute("note") Note note, BindingResult result) {
+        if (result.hasErrors()) {
+            return "redirect:/dashboard";
+        }
+        User user = userService.findUserByUsername(principal.getName());
+        noteService.addNote(user, note);
+        return "redirect:/dashboard";
+    }
+
+    @DeleteMapping(value = "/delete/{id}")
+    public String deletePlan(Principal principal, @PathVariable("id") Long id) {
+        noteService.deleteNote(id);
+        return "redirect:/dashboard";
+    }
+
+}
