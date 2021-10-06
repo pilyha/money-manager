8551f25c791a13076c7daa430545cde1099a39a7
illia <illia.popov57@gmail.com>
2021-10-07 00:08:16 +0300

    check travis ci and 1st try heroku

--- src/main/java/com/project/moneymanager/MoneymanagerApplication.java
+++ src/main/java/com/project/moneymanager/MoneymanagerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MoneymanagerApplication {
+    /*TEST*/
 
     public static void main(String[] args) {
         SpringApplication.run(MoneymanagerApplication.class, args);
